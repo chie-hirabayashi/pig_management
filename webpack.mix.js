1d566fba68eed8a33937e992fdfaa92d44fec1fb
“chie-hirabayashi” <nyantoroman@yahoo.co.jp>
2022-11-19 14:18:20 +0900

    feature_#9で追加したjsファイルは不要なため削除

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .js('resources/js/information.js', 'public/js')
+    // .js('resources/js/information.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require('tailwindcss'),
         require('autoprefixer'),
