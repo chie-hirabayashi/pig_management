f7fc156bb25a95e890b5ef278a0015e2d65ca85a
“chie-hirabayashi” <nyantoroman@yahoo.co.jp>
2023-02-21 00:23:16 +0900

    PlaceInライブワイヤ追加

---
+++ app/Http/Livewire/PlaceIn.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class PlaceIn extends Component
+{
+    public function render()
+    {
+        return view('livewire.place-in');
+    }
+}
