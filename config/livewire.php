45f07744ea9dd01ff0e8f575da9c6741f6a5731c
“chie-hirabayashi” <nyantoroman@yahoo.co.jp>
2023-02-26 00:23:26 +0900

    configを元に戻す&livewire.jsの読み込み

--- config/livewire.php
+++ config/livewire.php
@@ -54,8 +54,8 @@ return [
     |
     */
 
-    // 'asset_url' => null,
-    'asset_url' => '/assets',
+    'asset_url' => null,
+    // 'asset_url' => '/assets',
 
     /*
     |--------------------------------------------------------------------------
