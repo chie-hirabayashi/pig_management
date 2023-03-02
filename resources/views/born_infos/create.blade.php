86d1de47300e9d84038206f6925d28a36ca37a12
“chie-hirabayashi” <nyantoroman@yahoo.co.jp>
2023-03-03 00:31:19 +0900

    出産登録バグ修正

--- resources/views/born_infos/create.blade.php
+++ resources/views/born_infos/create.blade.php
@@ -57,7 +57,7 @@
                     <label class="text-sm text-gray-700" for="born_day">死産数</label>
                     <input type="number" name="stillbirtn_num" required
                         class="block px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring"
-                        placeholder="匹" value="{{ old('stillbirth_num') }}" min="1" max="20">
+                        placeholder="匹" value="{{ old('stillbirth_num') }}" min="0" max="20">
                 </div>
                 <!-- stillbirth_num - end -->
 
